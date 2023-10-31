46bfb0385fc813b78b52a67de08c615430815b50
Ian <ian1990@yandex.ru>
2023-10-31 15:35:46 +0300

    add tests

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -18,34 +18,34 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingFullDto createBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public ResponseEntity<BookingFullDto> createBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
                                         @Valid @RequestBody BookingDto booking) {
-        return bookingService.create(booking, userId);
+        return ResponseEntity.ok(bookingService.create(booking, userId));
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingFullDto updateBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public ResponseEntity<BookingFullDto> updateBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
                                  @PathVariable Long bookingId,
                                  @RequestParam String approved) {
-        return bookingService.update(approved, bookingId, userId);
+        return ResponseEntity.ok(bookingService.update(approved, bookingId, userId));
     }
 
     @GetMapping("/{bookingId}")
-    public BookingFullDto getBookingById(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public ResponseEntity<BookingFullDto> getBookingById(@RequestHeader("X-Sharer-User-Id") Long userId,
                               @PathVariable Long bookingId) {
-        return bookingService.get(userId, bookingId);
+        return ResponseEntity.ok(bookingService.get(userId, bookingId));
     }
 
     @GetMapping(params = "state")
-    public List<BookingFullDto> getBookingsByUser(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public ResponseEntity<List<BookingFullDto>> getBookingsByUser(@RequestHeader("X-Sharer-User-Id") Long userId,
                                            @RequestParam(defaultValue = "ALL") String state) {
-        return bookingService.getByUserIdAndState(userId, BookingState.from(state).orElseThrow(
+        return ResponseEntity.ok(bookingService.getByUserIdAndState(userId, BookingState.from(state).orElseThrow(
                 () -> new ValidationException("Unknown state: UNSUPPORTED_STATUS")
-        ));
+        )));
     }
 
     @GetMapping("/owner")
-    public List<BookingFullDto> getBookingsByItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public ResponseEntity<List<BookingFullDto>> getBookingsByItem(@RequestHeader("X-Sharer-User-Id") Long userId,
                                                   @RequestParam(required = false) String state,
                                                   @RequestParam(required = false) Long from,
                                                   @RequestParam(required = false) Long size) {
@@ -54,11 +54,14 @@ public class BookingController {
                     () -> new ValidationException("Unknown state: UNSUPPORTED_STATUS")
             );
         } else {
-            return bookingService.getAllBookingByItemsForUserId(userId, BookingState.ALL, from, size);
+            return ResponseEntity.ok(bookingService
+                    .getAllBookingByItemsForUserId(userId, BookingState.ALL, from, size));
         }
-        return bookingService.getAllBookingByItemsForUserId(userId, BookingState.from(state).orElseThrow(
-                () -> new ValidationException("Unknown state: UNSUPPORTED_STATUS")
-        ), from, size);
+        return ResponseEntity.ok(bookingService.getAllBookingByItemsForUserId(
+                userId,
+                BookingState.from(state)
+                        .orElseThrow(() -> new ValidationException("Unknown state: UNSUPPORTED_STATUS")),
+                from, size));
     }
 
     @GetMapping
--- src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingFullDto;
 
 import java.util.List;
@@ -24,10 +25,64 @@ class BookingControllerTest {
     private BookingController bookingController;
 
     @Test
-    void getAll_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+    void createBooking_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+        long userId = 0L;
+        BookingDto booking = new BookingDto();
+        BookingFullDto expectedBookingDto = new BookingFullDto();
+        when(bookingService.create(booking, userId)).thenReturn(expectedBookingDto);
+
+        ResponseEntity<BookingFullDto> response = bookingController.createBooking(userId, booking);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedBookingDto, response.getBody());
+    }
+
+    @Test
+    void updateBooking_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+        long userId = 0L;
+        Long bookingId = 0L;
+        String approved = "true";
+        BookingFullDto expectedBooking = new BookingFullDto();
+        when(bookingService.update(approved, bookingId, userId)).thenReturn(expectedBooking);
+
+        ResponseEntity<BookingFullDto> response = bookingController.updateBooking(userId, bookingId, approved);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedBooking, response.getBody());
+    }
+    @Test
+    void getBookingById_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+        long userId = 0L;
+        Long bookingId = 0L;
+        BookingFullDto expectedBooking = new BookingFullDto();
+        when(bookingService.get(userId, bookingId)).thenReturn(expectedBooking);
+
+        ResponseEntity<BookingFullDto> response = bookingController.getBookingById(userId, bookingId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedBooking, response.getBody());
+    }
+
+    @Test
+    void getBookingsByUser_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+        long userId = 0L;
+        String state = "ALL";
+        List<BookingFullDto> expectedBookingFullDto = List.of(new BookingFullDto());
+        when(bookingService.getByUserIdAndState(userId, BookingState.from(state).get()))
+                .thenReturn(expectedBookingFullDto);
+
+        ResponseEntity<List<BookingFullDto>> response = bookingController.getBookingsByUser(userId, state);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedBookingFullDto, response.getBody());
+    }
+
+    @Test
+    void getBookingsByItem_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
         long userId = 0L;
         Long from = 1L;
         Long size = 1L;
+        String state = "All";
         List<BookingFullDto> expectedBookingFullDto = List.of(new BookingFullDto());
         when(bookingService.getAllByUserId(userId, from, size)).thenReturn(expectedBookingFullDto);
 
@@ -36,4 +91,20 @@ class BookingControllerTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(expectedBookingFullDto, response.getBody());
     }
+    @Test
+    void getAll_whenInvoked_thenResponseStatusOkWithBookingDtoCollectionBody() {
+        long userId = 0L;
+        Long from = 1L;
+        Long size = 1L;
+        String state = "ALL";
+        List<BookingFullDto> expectedBookingFullDto = List.of(new BookingFullDto());
+        when(bookingService.getAllBookingByItemsForUserId(userId, BookingState.ALL, from, size))
+                .thenReturn(expectedBookingFullDto);
+
+        ResponseEntity<List<BookingFullDto>> response = bookingController.getBookingsByItem(userId, state, from, size);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedBookingFullDto, response.getBody());
+    }
+
 }
