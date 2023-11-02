b382c0556ec97a489518b4600cdbed723137867e
Ian <ian1990@yandex.ru>
2023-11-02 17:34:56 +0300

    checkstyle

--- src/test/java/ru/practicum/shareit/request/RequestServiceTest.java
+++ src/test/java/ru/practicum/shareit/request/RequestServiceTest.java
@@ -22,7 +22,6 @@ import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
